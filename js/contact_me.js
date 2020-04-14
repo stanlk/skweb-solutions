9470c4711a10c6b4b539744d52ee87d8f0dce547
Stan <stan@Stans-iMac.local>
2020-04-13 18:16:46 -0700

    Updated email for contact us

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url:'https://formspree.io/stanislav.kroschenko@gmail.com',
+        url:'https://formspree.io/xqkdwbpb',
         type: "POST",
         data: {
           name: name,
